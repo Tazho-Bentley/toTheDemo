d8c5b946236c487bf6268521a31195d2ca210cf3
Mukuma Musenge <kuma.musenge@gmail.com>
2018-03-06 23:19:06 +0200

    Added Entrust Package

--- config/app.php
+++ config/app.php
@@ -163,6 +163,12 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
+        /***
+         * Custom Service Providers
+         */
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -225,6 +231,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /**
+         * Custom Aliases
+         */
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
