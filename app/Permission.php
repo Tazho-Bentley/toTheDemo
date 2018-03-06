85153f2ff4280d172f6187d97f73a732de31ff8d
Mukuma Musenge <kuma.musenge@gmail.com>
2018-03-06 23:44:07 +0200

    Authentication Scaffolding

---
+++ app/Permission.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends EntrustPermission
+{
+    //
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permission = [
+        	[
+        		'name' => 'role-list',
+        		'display_name' => 'Display Role Listing',
+        		'description' => 'See only Listing Of Role'
+        	],
+        	[
+        		'name' => 'role-create',
+        		'display_name' => 'Create Role',
+        		'description' => 'Create New Role'
+        	],
+        	[
+        		'name' => 'role-edit',
+        		'display_name' => 'Edit Role',
+        		'description' => 'Edit Role'
+        	],
+        	[
+        		'name' => 'role-delete',
+        		'display_name' => 'Delete Role',
+        		'description' => 'Delete Role'
+        	]
+        ];
+
+
+        foreach ($permission as $key => $value) {
+        	Permission::create($value);
+        }
+    }
+}
