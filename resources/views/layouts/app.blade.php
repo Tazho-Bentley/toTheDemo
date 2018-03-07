99d0a086ccfcb90d75fd0622ddfdadcafa33ac77
Mukuma Musenge <kuma.musenge@gmail.com>
2018-03-07 15:34:08 +0200

    Layout Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,16 +51,6 @@
             <!-- END navbar header-->
             <!-- START Left navbar-->
             <ul class="navbar-nav mr-auto flex-row">
-               <li class="nav-item">
-                  <!-- Button used to collapse the left sidebar. Only visible on tablet and desktops-->
-                  <a class="nav-link d-none d-md-block d-lg-block d-xl-block" href="#" data-trigger-resize="" data-toggle-state="aside-collapsed">
-                     <em class="fa fa-navicon"></em>
-                  </a>
-                  <!-- Button to show/hide the sidebar on mobile. Visible on mobile only.-->
-                  <a class="nav-link sidebar-toggle d-md-none" href="#" data-toggle-state="aside-toggled" data-no-persist="true">
-                     <em class="fa fa-navicon"></em>
-                  </a>
-               </li>
                <!-- START User avatar toggle-->
                <li class="nav-item d-none d-md-block">
                   <!-- Button used to collapse the left sidebar. Only visible on tablet and desktops-->
@@ -69,108 +59,28 @@
                   </a>
                </li>
                <!-- END User avatar toggle-->
-               <!-- START lock screen-->
-               <li class="nav-item d-none d-md-block">
-                  <a class="nav-link" href="lock.html" title="Lock screen">
-                     <em class="icon-lock"></em>
-                  </a>
-               </li>
-               <!-- END lock screen-->
             </ul>
             <!-- END Left navbar-->
             <!-- START Right Navbar-->
             <ul class="navbar-nav flex-row">
-               <!-- Search icon-->
-               <li class="nav-item">
-                  <a class="nav-link" href="#" data-search-open="">
-                     <em class="icon-magnifier"></em>
-                  </a>
-               </li>
-               <!-- Fullscreen (only desktops)-->
-               <li class="nav-item d-none d-md-block">
-                  <a class="nav-link" href="#" data-toggle-fullscreen="">
-                     <em class="fa fa-expand"></em>
-                  </a>
-               </li>
-               <!-- START Alert menu-->
-               <li class="nav-item dropdown dropdown-list">
-                  <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-toggle="dropdown">
-                     <em class="icon-bell"></em>
-                     <span class="badge badge-danger">11</span>
-                  </a>
-                  <!-- START Dropdown menu-->
-                  <div class="dropdown-menu dropdown-menu-right animated flipInX">
-                     <div class="dropdown-item">
-                        <!-- START list group-->
-                        <div class="list-group">
-                           <!-- list item-->
-                           <div class="list-group-item list-group-item-action">
-                              <div class="media">
-                                 <div class="align-self-start mr-2">
-                                    <em class="fa fa-twitter fa-2x text-info"></em>
-                                 </div>
-                                 <div class="media-body">
-                                    <p class="m-0">New followers</p>
-                                    <p class="m-0 text-muted text-sm">1 new follower</p>
-                                 </div>
-                              </div>
-                           </div>
-                           <!-- list item-->
-                           <div class="list-group-item list-group-item-action">
-                              <div class="media">
-                                 <div class="align-self-start mr-2">
-                                    <em class="fa fa-envelope fa-2x text-warning"></em>
-                                 </div>
-                                 <div class="media-body">
-                                    <p class="m-0">New e-mails</p>
-                                    <p class="m-0 text-muted text-sm">You have 10 new emails</p>
-                                 </div>
-                              </div>
-                           </div>
-                           <!-- list item-->
-                           <div class="list-group-item list-group-item-action">
-                              <div class="media">
-                                 <div class="align-self-start mr-2">
-                                    <em class="fa fa-tasks fa-2x text-success"></em>
-                                 </div>
-                                 <div class="media-body">
-                                    <p class="m-0">Pending Tasks</p>
-                                    <p class="m-0 text-muted text-sm">11 pending task</p>
-                                 </div>
-                              </div>
-                           </div>
-                           <!-- last list item-->
-                           <div class="list-group-item list-group-item-action">
-                              <span class="d-flex align-items-center">
-                                 <span class="text-sm">More notifications</span>
-                                 <span class="badge badge-danger ml-auto">14</span>
-                              </span>
-                           </div>
-                        </div>
-                        <!-- END list group-->
-                     </div>
-                  </div>
-                  <!-- END Dropdown menu-->
-               </li>
                <!-- END Alert menu-->
                <!-- START Offsidebar button-->
-               <li class="nav-item">
-                  <a class="nav-link" href="#" data-toggle-state="offsidebar-open" data-no-persist="true">
-                     <em class="icon-notebook"></em>
-                  </a>
-               </li>
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
                <!-- END Offsidebar menu-->
             </ul>
             <!-- END Right Navbar-->
-            <!-- START Search form-->
-            <form class="navbar-form" role="search" action="search.html">
-               <div class="form-group">
-                  <input class="form-control" type="text" placeholder="Type and hit enter ...">
-                  <div class="fa fa-times navbar-form-close" data-search-dismiss=""></div>
-               </div>
-               <button class="d-none" type="submit">Submit</button>
-            </form>
-            <!-- END Search form-->
          </nav>
          <!-- END Top Navbar-->
       </header>
@@ -194,14 +104,25 @@
                            </div>
                            <!-- Name and Job-->
                            <div class="user-block-info">
-                              <span class="user-block-name">Hello, Mike</span>
-                              <span class="user-block-role">Designer</span>
+                              <span class="user-block-name">Hello, {{Auth::User()->name}}</span>
+                              <span class="user-block-role">{{Auth::User()->email}}</span>
                            </div>
                         </div>
                      </div>
                   </li>
                   <!-- END user info-->
                   <!-- Iterates over all sidebar items-->
+                  @if (!Auth::guest())
+                  <li class="nav-heading ">
+                     <span data-localize="sidebar.heading.HEADER">Dashboard</span>
+                  </li>
+                  <li class=" ">
+                     <a href="{{route('home')}}" title="Widgets">
+                        <em class="icon-grid"></em>
+                        <span data-localize="sidebar.nav.WIDGETS">Information</span>
+                     </a>
+                  </li>
+                  @role('Administrator')
                   <li class="nav-heading ">
                      <span data-localize="sidebar.heading.HEADER">Accounts</span>
                   </li>
@@ -223,6 +144,8 @@
                         <span data-localize="sidebar.nav.WIDGETS">Manage POS</span>
                      </a>
                   </li>
+                  @endrole
+                  @role('super-user')
                   <li class="nav-heading ">
                      <span data-localize="sidebar.heading.HEADER">SU Setups</span>
                   </li>
@@ -244,7 +167,9 @@
                         <span data-localize="sidebar.nav.WIDGETS">Manage System Permissions</span>
                      </a>
                   </li>
+                  @endrole
                </ul>
+               @endif
                <!-- END sidebar nav-->
             </nav>
          </div>
